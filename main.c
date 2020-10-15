7a2c02f85b2d18327ce349e6778ba94a4ca3796f
kway2001 <70741593+kway2001@users.noreply.github.com>
2020-10-15 16:01:21 +0900

    practice 4

--- main.c
+++ main.c
@@ -3,20 +3,22 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+void f(void);
+int i;
 
-int inc(int counter)
-{
-	counter++;
-	return counter;
-}
 
 int main(int argc, char *argv[]) {
 	
-	int i=0;
-	
-	printf("before function call i = %d\n", i);
-	inc(i);
-	printf("after function call i = %d\n", i);
+	for (i=0; i<5; i++)
+	{
+		f();
+	}
 	
 	return 0;
+}
+
+void f(void)
+{
+	for (i=0; i<10; i++)
+		printf("#");
 }
